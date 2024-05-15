9fe036424a3ad55f86e865869aa8ffcba4f9ccf7
takuma <k023g0015@g.neec.ac.jp>
2024-05-15 22:05:49 +0900

    MyMath.hつくったyo

--- DirectXGame/MyMath.h
+++ DirectXGame/MyMath.h
@@ -155,6 +155,46 @@ public:
 		return result;
 	}
 
+	Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
+		Matrix4x4 result;
+
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				result.m[i][j] = m1.m[i][j] + m2.m[i][j];
+			}
+		}
+
+		return result;
+	}
+
+	Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2) {
+		Matrix4x4 result;
+
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				result.m[i][j] = m1.m[i][j] - m2.m[i][j];
+			}
+		}
+
+		return result;
+	}
+
+	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+		Matrix4x4 result;
+
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				result.m[i][j] = 0;
+				for (int k = 0; k < 4; ++k) {
+					result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+				}
+			}
+		}
+
+		return result;
+	}
+
+
 	Matrix4x4 MakeIdentity4x4() {
 
 		Matrix4x4 identity;
