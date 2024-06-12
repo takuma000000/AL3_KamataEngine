dffc3a39ac4ef4d71899ab8137e2f9c897ec149a
takuma <k023g0015@g.neec.ac.jp>
2024-06-12 22:42:56 +0900

    シーンから

--- DirectXGame/CameraController.cpp
+++ DirectXGame/CameraController.cpp
@@ -8,7 +8,15 @@ void CameraController::Initialize() {
 
 }
 
-void CameraController::Update() {}
+void CameraController::Update() {
+
+	const WorldTransform& targetWorldTransform = target_->GetWorldTransform();
+
+	viewProjection_.translation_ = targetWorldTransform.translation_ + targetOffest_;
+
+	viewProjection_.UpdateMatrix();
+
+}
 
 void CameraController::Reset() {
 
