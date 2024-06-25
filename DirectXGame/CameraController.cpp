f4384c3bc4070cb887f89fe16dee835e6a803bbd
takuma <k023g0015@g.neec.ac.jp>
2024-06-26 00:50:23 +0900

    02_08 吸着から

--- DirectXGame/CameraController.cpp
+++ DirectXGame/CameraController.cpp
@@ -40,12 +40,23 @@ void CameraController::Update() {
 	//座標補間によりゆったり追従
 	viewProjection_.translation_ = Lerp(viewProjection_.translation_, mark_, kInterpolationRate);
 
+	//追従対象が画面外に出ないように補正
+	viewProjection_.translation_.x =
+	    std::max(viewProjection_.translation_.x, targetWorldTransform.translation_.x + margin.left);
+	viewProjection_.translation_.x =
+	    std::min(viewProjection_.translation_.x, targetWorldTransform.translation_.x + margin.right);
+	viewProjection_.translation_.y =
+	    std::max(viewProjection_.translation_.y, targetWorldTransform.translation_.y + margin.bottom);
+	viewProjection_.translation_.y = std::min(
+	    viewProjection_.translation_.y, targetWorldTransform.translation_.y + margin.top);
+
 	//移動範囲制限
 	viewProjection_.translation_.x = std::min(viewProjection_.translation_.x, movableArea_.left);
 	viewProjection_.translation_.x = std::max(viewProjection_.translation_.x, movableArea_.right);
 	viewProjection_.translation_.y = std::min(viewProjection_.translation_.y, movableArea_.bottom);
 	viewProjection_.translation_.y = std::max(viewProjection_.translation_.y, movableArea_.top);
 
+	//行列を更新する
 	viewProjection_.UpdateMatrix();
 
 	
